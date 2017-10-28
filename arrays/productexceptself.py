404575345ed227b856a0b90046e39b2acb3a704c
Rahul Deshmukh <rahul.a.deshmukh@gmail.com>
2017-10-28 12:40:15 -0400

    added comments productsexceptself

--- arrays/productexceptself.py
+++ arrays/productexceptself.py
@@ -1,3 +1,15 @@
+"""
+Problem: 
+Given an array, return an array which at a particular index returns product of all other numbers in the original array other than itself 
+
+Algorithm & Solution: 
+Explanation is quite simple and best explained through code comments. Please look below 
+
+Time Complexity: O(n)
+Space Complexity: O(n) 
+"""
+print(__doc__)
+
 class SolutionProductExceptSelf(object):
     def productExceptSelf(self, nums):
         """
@@ -8,16 +20,22 @@ class SolutionProductExceptSelf(object):
         if not nums or len(nums) == 1:
             return nums 
         
+        # Create new array to hold results
         output = [0] * len(nums)
         
         result = 1
-        # Starting left side set value for each output element by multiplying previous element
+        # For input array [3, 4, 5] 
+        # output will look like [1, 3, 12]
+        # at the end of this loop. Notice how last element already has the
+        # right result and first element has 1
         for i in range(len(nums)):
             output[i] = result 
             result *= nums[i]
         
         result = 1
-        # Starting right side set value for each output element by multiplying previous element 
+        # Now iterate from right to left 
+        # Now output will look like [20, 15, 12]
+        # result we are looking for  
         for i in reversed(range(len(nums))):
             output[i] *= result 
             result *= nums[i]
@@ -26,5 +44,11 @@ class SolutionProductExceptSelf(object):
 
 if __name__ == '__main__':
     s = SolutionProductExceptSelf()
-    print(s.productExceptSelf([2,3,1,4]))
+    array = [3, 4, 5]
+    print("Input= {}".format(array))
+    print("Result= {}".format(s.productExceptSelf(array)))
+    print() 
+    array = [2,3,1,4]
+    print("Input= {}".format(array))
+    print("Result= {}".format(s.productExceptSelf(array)))
     
