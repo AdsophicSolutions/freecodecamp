d22812ca7989d6c339d5a286795f65496d290005
Rahul Deshmukh <rahul.a.deshmukh@gmail.com>
2017-10-28 14:33:33 -0400

    updated maxsubarrayproduct and maxsubarraysum.

--- arrays/maxsubarraryproduct.py
+++ arrays/maxsubarraryproduct.py
@@ -1,47 +1,42 @@
+"""
+Problems: 
+Given an array return the max sub-array product. Array can have both positive and negative numbers
+
+Algorithm:
+Trick here is to maintain a largest running negative and running positive and update max_product whenever max_product exceeds previously calculated max_product. 
+
+Solution: 
+To start set max_product, running_positive_running_negative to first element in array. Now for rest of the array update running_positive as max between current number, current number * running_positive and current_number and * running_negative. Similarly, running_negative is min between current_number, current_number * running_positive and current_number * running_negative. If running_positive exceeds max_product update it.  
+
+Time Complexity: O(n) 
+Space Complexity: O (1) 
+"""
+print(__doc__)
+
+import itertools
+
 class SolutionSubArrayProduct(object):
     def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """        
-        running_positive = None if nums[0] < 0 else nums[0]
-        running_negative = 0 if nums[0] > 0 else nums[0]
-        max_product = nums[0]
-        
-        for i in range(1, len(nums)):
-            if nums[i] == 0:
-                running_positive = 0 
-                running_negative = 0
-            # if nums[i] > 0 multiply running_positive by this current number 
-            # Multiply running negative only if current value is less than 0 
-            elif nums[i] > 0:
-                running_positive = nums[i] if not running_positive else running_positive * nums[i]    
-                if running_negative < 0:
-                    running_negative = running_negative * nums[i]
-            else: 
-                # current number is < 0. 
-                # save running positive value
-                temp = running_positive
-                running_positive = running_negative * nums[i] 
-                running_negative = nums[i] if not temp else temp * nums[i]
-                
-            if running_positive is not None and running_positive > max_product: 
-                max_product = running_positive
-            
-        return max_product
-    
-    def maxProduct1(self, nums):
         max_product = running_positive = running_negative = nums[0]
-        for num in nums[1:]:
+        # Use itertools.islice instead of nums[1:] since it creates a 
+        # new array 
+        for num in itertools.islice(nums, 1, None):
+            # update running_positive and running_negative
             running_positive, running_negative = (
                 max(num, num * running_positive, num * running_negative ),
                 min(num, num * running_positive, num * running_negative ))
             
+            # update max_product if running_positive is greater 
             max_product = max(max_product, running_positive)
         
         return max_product
 
 if __name__ == '__main__':
     s = SolutionSubArrayProduct()
-    print(s.maxProduct([2, -3, 3, 2]))
-    print(s.maxProduct1([2, -3, 3, 2]))
+    array = [2, -3, 3, 1, 2]
+    print("Input= {}".format(array))
+    print("Answer= {}".format(s.maxProduct(array)))
+    print()
+    array = [-3, 5, -4]
+    print("Input= {}".format(array))
+    print("Answer= {}".format(s.maxProduct(array)))
--- arrays/maxsubarraysum.py
+++ arrays/maxsubarraysum.py
@@ -1,3 +1,23 @@
+"""
+Problem: 
+Given an array return maximum sub-array sum. Array contains integers 
+
+Algorithm & Solution: 
+Important concept to grasp here is while traversing the array if value for a particular element is greater running sum of elements before it + element value, we can make value of this element the running sum. Let's consider an example:
+ 
+-3 -2 1 are first three elements of an array
+when we arrive at index 2 (element value 1) our running sum is -5. If we add 1 to our running sum our new running_sum becomes -4. The other option is to make 1 our new running sum. We must choose the second option because any subsequent running_sum will be greater if we start with 1 instead of -4. 
+
+Now that we know what to use as the running sum, we just need to keep track of a max_sum, which is the largest running sum so far.  
+
+Time Complexity: O(n) 
+Space Complexity: O(1)
+"""
+import itertools
+from functools import reduce  
+
+print(__doc__)
+
 class SolutionMaxSubArray(object):
     def maxSubArray(self, nums):
         """
@@ -7,24 +27,42 @@ class SolutionMaxSubArray(object):
         if not nums:
             return 0 
                 
-        max_sum = nums[0]
-        cur_sum = nums[0]
-        for i in range(1, len(nums)):
-            # nums[i] + cur_sum is greater than num[i]
-            # add nums[i] to running sum else
-            # nums[i] becomes the new running sum 
-            if nums[i] + cur_sum > nums[i]:
-                cur_sum += nums[i]
-            else:
-                cur_sum = nums[i]
-            
-            # if running sum cur_sum > max_sum we have found
+        max_sum = running_sum = nums[0]
+        
+        for num in itertools.islice(nums, 1, None):
+            # if running_sum
+            running_sum = max(running_sum + num, num)
+              
+            # if running sum running_sum > max_sum we have found
             # a new max_sum 
-            if cur_sum > max_sum:
-                max_sum = cur_sum
+            max_sum = max(max_sum, running_sum)
         
         return max_sum 
     
+    def maxSubArrayReduce(self, nums):
+        """
+        Advanced method 
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0 
+        
+        # Demonstration of reduce could be used to perform the same 
+        # function
+        return reduce(lambda max_t, num: (
+            max(max_t[0] + num, num), 
+            max(max_t[1], max(max_t[0] + num, num))), 
+                      itertools.islice(nums, 1, None), (nums[0], nums[0]))[1]
+    
 if __name__ == '__main__':
-    s = SolutionMaxSubArray()
-    print(s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+    s = SolutionMaxSubArray()    
+    array = [1, 2, 3, 4]
+    print("Input= {}".format(array))    
+    print("Answer= {}".format(s.maxSubArray(array)))
+    print("Answer= {}".format(s.maxSubArrayReduce(array)))
+    print() 
+    array = [-2,1,-3,4,-1,2,1,-5,4]
+    print("Input= {}".format(array))    
+    print("Answer= {}".format(s.maxSubArray(array)))
+    print("Answer= {}".format(s.maxSubArrayReduce(array)))
