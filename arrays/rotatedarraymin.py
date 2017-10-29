f3ed1b24530c86156946486d6fd9c0496f631f8a
Rahul Deshmukh <rahul.a.deshmukh@gmail.com>
2017-10-29 08:16:06 -0400

    Added comments to rotatedarraymin and rotatedarray search

--- arrays/rotatedarraymin.py
+++ arrays/rotatedarraymin.py
@@ -1,3 +1,26 @@
+"""
+Problem: 
+Given an pivoted sorted array, find the minimum element in the array. Search must be must perform in O(log n) 
+
+Algorithm: 
+We know that if start index, mid index and end index elements are in increasing order, the start index has the minimum element in the array. We perform a binary search on our array and look for the condition to be true. If condition is not True, we compare value at start index to value at middle index. If value at start index is <= value at mid index, the smallest element exists on the right side of the middle element, else smallest element lies to the left. 
+
+Solution: 
+Debug code below with a few examples to understand it well. 
+Here are a couple of tips to remember what operators to use. 
+1. In statement 
+    if nums[si] <= nums[mid] <= nums[ei] 
+    we must use <= and not < because if we were testing an array of size 1, si = mid = ei and that means < operator would fail.
+2. In two statements 
+    if nums[si] <= nums[mid]:
+        si = mid + 1
+    remember, if we were testing an array of size 2, si = mid = 0 so we need to use <= operator not < operator. In the second statement si must be set to mid + 1 since for an array of size 2, si = mid = 0, and we si = 1 for next iteration
+
+Time Complexity: O(log n) 
+Space Complexity: O(1)
+"""
+print(__doc__)
+
 class SolutionFindMin(object):
     def findMin(self, nums):
         """
@@ -7,36 +30,40 @@ class SolutionFindMin(object):
         if not nums:
             return 0 
         
-        si = 0 
-        ei = len(nums) - 1
-        while si < ei: 
-            # if nums[si] is less than nums[ei]. This portion is sorted and 
-            # so return value at si as the smallest value 
-            if nums[si] < nums[ei]:
-                break  
-            
-            # if si + 1 == ei and value at ei is lower, value at ei must be the first 
-            # element in the rotated array and hence the smallest value 
-            if si + 1 == ei and nums[si] > nums[ei]:
-                si += 1
-                break
-            
-            # find mid    
-            mid = (si + ei) // 2
-            
-            # If value at si is less than value at mid 
-            # since we've already established array isn't fully sorted (nums[si] > nums[ei])
-            # we now mid is to the left of pivot point. hence si = mid 
-            # if value at mid is higher, mid is to right of pivot point. 
-            # make ei = mid 
-            if nums[si] < nums[mid]: 
-                si = mid
-            else: 
-                ei = mid
+        # Init start and end index
+        si, ei = 0, len(nums) - 1
         
-        return nums[si] 
+        while si <= ei:
+            mid = (si + ei) // 2   
+            # if values at start, mid and end are sorted start index value is minimum break and return  
+            # Why do we use <= instead of just <? Because eventually 
+            # we might be checking a sub-array of size 1 or 2 at which point 
+            # 2 or more indexes are equal.      
+            if nums[si] <= nums[mid] <= nums[ei]:
+                break 
+
+            # Go to the right side of the array if left side is sorted.
+            # Remember: use <= and not < because when comparing an array of 
+            # size 2 si = mid = 0  
+            if nums[si] <= nums[mid]:
+                # Tip for remembering operator: 
+                # for array of 2, first iteration yields si = mid = 0 
+                # next iteration must move si to mid + 1
+                si = mid + 1
+            # Otherwise go to the left. 
+            else:
+                ei = mid
+                
+
+        return nums[si]
 
 if __name__ == '__main__':
     s = SolutionFindMin()
-    print(s.findMin([3, 4, 5, 1, 2]))
-    print(s.findMin([5, 1 ,2 ,3, 4]))
+    array = [3, 4, 5, 1, 2]
+    print("Input: Array = {}".format(array))
+    print("Answer: Minimum Element = {}".format(s.findMin(array)))
+    print()    
+    array = [5, 1 ,2 ,3, 4]
+    print("Input: Array = {}".format(array))
+    print("Answer: Minimum Element = {}".format(s.findMin(array)))
+    
--- arrays/rotatedarraysearch.py
+++ arrays/rotatedarraysearch.py
@@ -1,10 +1,24 @@
+"""
+Problem: 
+Given a sorted array pivoted at some point search for a target value. Return index of target value and -1 if target does not exist. Search must be performed in O(log n) 
+
+Algorithm: 
+Let's start with an example array [4, 5, 1, 2, 3] pivoted at index 2 (value = 1). We are searching for target value of 5. If we consider the whole array where si = 0 and ei = length(array) - 1 = 4 our array mid point is index = 2. If value at start index <= value at mid point, we know that left side of the array is sorted, otherwise we know right side of array is sorted. In our example,  we compare array[0] (value = 4) to array[2] (value = 1). We now know left side of array is not sorted which means right side of array must be. Now we can check if our target value lies this right side sorted array. That means checking if array[2] <= target <= array[4] translating to (1 <= 5 <= 3). This condition is false, so we know target value can possibly only exist on left side of the array. Move ei = mid - 1 and continue the search 
+
+Solution: 
+Please debug a few examples of solution to understand algorithm. Tips to remember - in statement if nums[si] <= nums[mid]: remember to use <= and not < because if we were searching in an array of size 2 si and mid are both equal to 0.  
+
+Time Complexity: O(log n) 
+Space Complexity: O(1) 
+"""
+print(__doc__)
+
 class SolutionArraySearch(object):
     def search(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
-        :rtype: int
-        Performed in O(Log n) efficiency         
+        :rtype: int                 
         """
         if not nums: 
             return -1
@@ -25,7 +39,7 @@ class SolutionArraySearch(object):
             # Left side of the array is sorted since nums[si] <= nums[mid]
             if nums[si] <= nums[mid]:
                 # Since left side is sorted we can check for value on the left side 
-                if nums[si] <= target and nums[mid] >= target:
+                if nums[si] <= target <= nums[mid]:
                     ei = mid - 1
                 # Value must exist on the right side. 
                 else:
@@ -33,7 +47,7 @@ class SolutionArraySearch(object):
             # Right side is sorted
             else:
                 # Check if value lies between mid and end. 
-                if nums[mid] <= target and nums[ei] >= target:
+                if nums[mid] <= target <= nums[ei]:
                     si = mid + 1
                 # Move search to left side 
                 else:
@@ -41,10 +55,23 @@ class SolutionArraySearch(object):
         
         return location
             
-            
 
 if __name__ == '__main__':
     s = SolutionArraySearch()
-    print(s.search([4, 5, 1, 2, 3], 2))
-    print(s.search([5, 1, 2, 3, 2], 2))
-    print(s.search([5, 1, 2, 3, 2], 9))
+    array = [4, 5, 1, 2, 3]
+    target = 2
+    print("Input: Array = {}, Target = {}".format(array, target))
+    print("Answer: Target Index = {}".format(s.search(array, target)))
+    print() 
+    
+    array = [5, 1, 2, 3, 4]
+    target = 2
+    print("Input: Array = {}, Target = {}".format(array, target))
+    print("Answer: Target Index = {}".format(s.search(array, target)))
+    print() 
+    
+    array = [5, 1, 2, 3, 4]
+    target = 9
+    print("Input: Array = {}, Target = {}".format(array, target))
+    print("Answer: Target Index = {}".format(s.search(array, target)))
+    
